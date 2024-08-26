68449bd7df6dbc4bc0257fa744f60ea856b607d7
deepak singh <djangoboy.18feb@gmail.com>
2024-08-26 20:04:09 +0530

    published the package to npm

---
+++ vanila_js_sites/no_prior_authentication/package-consumer/node_modules/chathead-consumer-client/dist/main.79ebb3b18ea92c2d0279.hot-update.js
@@ -0,0 +1,30 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+this["webpackHotUpdaterandomColorPackage"]("main",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js!./src/style.css":
+/*!*************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./src/style.css ***!
+  \*************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/noSourceMaps.js */ \"./node_modules/css-loader/dist/runtime/noSourceMaps.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);\n// Imports\n\n\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, `/* Basic styling */\nbody, html {\n    margin: 0;\n    padding: 0;\n    font-family: Arial, sans-serif;\n}\n\n/* Header styling */\n.header {\n    display: flex;\n    justify-content: space-between;\n    align-items: center;\n    background-color: red;\n    color: white;\n    height: 60px; /* Adjust the height as needed */\n    padding: 0 20px;\n}\n\n.header .left {\n    font-size: 24px;\n}\n\n.header .right {\n    display: flex;\n    align-items: center;\n}\n\n.header button {\n    margin-left: 10px;\n    padding: 5px 10px;\n}\n\n\n#chat_modal_opener\n\n\n.chat_modal {\n\n    display: none;\n    max-height: 600px;\n    width: 400px;\n    /* border: 1px solid red; */\n    border-radius: 10px;\n    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);\n    font-family: Arial, sans-serif;\n    background-color: #f9f9f9;\n    position: fixed;\n    bottom: 20px;\n    right: 20px;\n    /* display: flex; */\n    flex-direction: column;\n    overflow: auto; /* Add this line to enable scrollbar */\n    -ms-overflow-style: none;  /* IE and Edge */\n    scrollbar-width: none;  /* Firefox */\n\n}\n\n/* Hide scrollbar for WebKit browsers (Chrome, Safari) */\n.chat_modal::-webkit-scrollbar {\n    display: none;\n}\n\n.chat_header {\n    padding: 10px;\n    background-color: #f1f1f1;\n    border-bottom: 1px solid #ccc;\n    border-top-left-radius: 10px;\n    border-top-right-radius: 10px;\n    text-align: center;\n}\n\n.chat_body {\n    padding: 10px;\n    flex: 1;\n    overflow-y: auto;\n}\n\n.message {\n    padding: 10px;\n    border-radius: 10px;\n    margin-bottom: 10px;\n    max-width: 80%;\n}\n\n.admin {\n    background-color: #e0e0e0;\n    align-self: flex-start;\n}\n\n.user {\n    background-color: #6200ea;\n    color: white;\n    align-self: flex-end;\n}\n\n.chat_footer {\n    display: flex;\n    padding: 10px;\n    border-top: 1px solid #ccc;\n}\n\n.chat_footer input {\n    flex: 1;\n    padding: 10px;\n    border: 1px solid #ccc;\n    border-radius: 10px;\n    margin-right: 10px;\n}\n\n.chat_footer button {\n    padding: 10px 20px;\n    background-color: #6200ea;\n    color: white;\n    border: none;\n    border-radius: 10px;\n    cursor: pointer;\n}\n\n.chat_footer button:hover {\n    background-color: #5200ca;\n}\n\n.time {\n    display: block;\n    font-size: 0.75em;\n    text-align: right;\n    color: #777;\n}\n\n/* Existing CSS */\n\n/* CSS for the full-screen div */\n.full-screen-div {\n    display: flex;\n    width: 100%;\n    height: 100vh;\n    background-color: gray;\n}\n\n/* CSS for the left part */\n.left-part {\n    width: 35%;\n    height: 100%;\n    background-color: green;\n}\n\n/* CSS for the right part */\n.right-part {\n    width: 60%;\n    height: 100%;\n    background-color: red;\n    margin-left: 5%;\n}\n\n\n`, \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://randomColorPackage/./src/style.css?./node_modules/css-loader/dist/cjs.js");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("623b54cebc1a1bbc98c8")
+/******/ })();
+/******/ 
+/******/ }
+);
---
+++ vanila_js_sites/no_prior_authentication/package-consumer/node_modules/chathead-consumer-client/dist/main.7e33b9b08699c1e42f5d.hot-update.js
@@ -0,0 +1,50 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+this["webpackHotUpdaterandomColorPackage"]("main",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js!./src/style.css":
+/*!*************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./src/style.css ***!
+  \*************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/noSourceMaps.js */ \"./node_modules/css-loader/dist/runtime/noSourceMaps.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);\n// Imports\n\n\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, `/* Basic styling */\nbody, html {\n    margin: 0;\n    padding: 0;\n    font-family: Arial, sans-serif;\n}\n\n/* Header styling */\n.header {\n    display: flex;\n    justify-content: space-between;\n    align-items: center;\n    background-color: red;\n    color: white;\n    height: 60px; /* Adjust the height as needed */\n    padding: 0 20px;\n}\n\n.header .left {\n    font-size: 24px;\n}\n\n.header .right {\n    display: flex;\n    align-items: center;\n}\n\n.header button {\n    margin-left: 10px;\n    padding: 5px 10px;\n}\n\n/* Modal styling */\n.modal {\n    display: none;\n    position: absolute;\n    top: 60px; /* Adjust based on the header height */\n    right: 0; /* Adjust based on the header height */\n    width: 200px;\n    height: calc(90% - 60px); /* Adjust based on the header height */\n    background-color: #ffffff;\n    border: 1px solid #ccc;\n    box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);\n    padding: 20px;\n    z-index: 1000;\n}\n\n\n/* Modal styling */\n.chat_modal {\n    display: none;\n    position: absolute;\n    bottom: 10px; /* Adjust based on the header height */\n    right: 100px; /* Adjust based on the header height */\n    width: 400px;\n    height: calc(60% - 60px); /* Adjust based on the header height */\n    background-color: #ffffff;\n    border: 1px solid #ccc;\n    box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);\n    padding: 20px;\n    z-index: 1000;\n}\n\n\n/* Existing CSS */\n\n/* CSS for the full-screen div */\n.full-screen-div {\n    display: flex;\n    width: 100%;\n    height: 100vh;\n    background-color: gray;\n}\n\n/* CSS for the left part */\n.left-part {\n    width: 35%;\n    height: 100%;\n    background-color: green;\n}\n\n/* CSS for the right part */\n.right-part {\n    width: 60%;\n    height: 100%;\n    background-color: red;\n    margin-left: 5%;\n}\n\n\n`, \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://randomColorPackage/./src/style.css?./node_modules/css-loader/dist/cjs.js");
+
+/***/ }),
+
+/***/ "./node_modules/css-loader/dist/runtime/api.js":
+/*!*****************************************************!*\
+  !*** ./node_modules/css-loader/dist/runtime/api.js ***!
+  \*****************************************************/
+/***/ ((module) => {
+
+eval("\n\n/*\n  MIT License http://www.opensource.org/licenses/mit-license.php\n  Author Tobias Koppers @sokra\n*/\nmodule.exports = function (cssWithMappingToString) {\n  var list = [];\n\n  // return the list of modules as css string\n  list.toString = function toString() {\n    return this.map(function (item) {\n      var content = \"\";\n      var needLayer = typeof item[5] !== \"undefined\";\n      if (item[4]) {\n        content += \"@supports (\".concat(item[4], \") {\");\n      }\n      if (item[2]) {\n        content += \"@media \".concat(item[2], \" {\");\n      }\n      if (needLayer) {\n        content += \"@layer\".concat(item[5].length > 0 ? \" \".concat(item[5]) : \"\", \" {\");\n      }\n      content += cssWithMappingToString(item);\n      if (needLayer) {\n        content += \"}\";\n      }\n      if (item[2]) {\n        content += \"}\";\n      }\n      if (item[4]) {\n        content += \"}\";\n      }\n      return content;\n    }).join(\"\");\n  };\n\n  // import a list of modules into the list\n  list.i = function i(modules, media, dedupe, supports, layer) {\n    if (typeof modules === \"string\") {\n      modules = [[null, modules, undefined]];\n    }\n    var alreadyImportedModules = {};\n    if (dedupe) {\n      for (var k = 0; k < this.length; k++) {\n        var id = this[k][0];\n        if (id != null) {\n          alreadyImportedModules[id] = true;\n        }\n      }\n    }\n    for (var _k = 0; _k < modules.length; _k++) {\n      var item = [].concat(modules[_k]);\n      if (dedupe && alreadyImportedModules[item[0]]) {\n        continue;\n      }\n      if (typeof layer !== \"undefined\") {\n        if (typeof item[5] === \"undefined\") {\n          item[5] = layer;\n        } else {\n          item[1] = \"@layer\".concat(item[5].length > 0 ? \" \".concat(item[5]) : \"\", \" {\").concat(item[1], \"}\");\n          item[5] = layer;\n        }\n      }\n      if (media) {\n        if (!item[2]) {\n          item[2] = media;\n        } else {\n          item[1] = \"@media \".concat(item[2], \" {\").concat(item[1], \"}\");\n          item[2] = media;\n        }\n      }\n      if (supports) {\n        if (!item[4]) {\n          item[4] = \"\".concat(supports);\n        } else {\n          item[1] = \"@supports (\".concat(item[4], \") {\").concat(item[1], \"}\");\n          item[4] = supports;\n        }\n      }\n      list.push(item);\n    }\n  };\n  return list;\n};\n\n//# sourceURL=webpack://randomColorPackage/./node_modules/css-loader/dist/runtime/api.js?");
+
+/***/ }),
+
+/***/ "./node_modules/css-loader/dist/runtime/noSourceMaps.js":
+/*!**************************************************************!*\
+  !*** ./node_modules/css-loader/dist/runtime/noSourceMaps.js ***!
+  \**************************************************************/
+/***/ ((module) => {
+
+eval("\n\nmodule.exports = function (i) {\n  return i[1];\n};\n\n//# sourceURL=webpack://randomColorPackage/./node_modules/css-loader/dist/runtime/noSourceMaps.js?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("da8966d4f58fa4dee6cb")
+/******/ })();
+/******/ 
+/******/ }
+);
