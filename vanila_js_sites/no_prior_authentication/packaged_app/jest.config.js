e2143e63c66ff4ce3c4148ed06e6a248ecefe74d
codekeep18feb <codekeep18feb@gmail.com>
2024-09-20 22:17:20 +0530

    splitted away the package from example repo with no prior auth

--- vanila_js_sites/no_prior_authentication/packaged_app/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  moduleNameMapper: {
-      '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',  // Mock CSS
-      '\\.(jpg|jpeg|png)$': '<rootDir>/__mocks__/fileMock.js',  // Mock image files
-  },
-};
