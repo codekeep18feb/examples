0f3b8d6cacec5b26db3ed13fbd62e976ffbe619e
codekeep18feb <codekeep18feb@gmail.com>
2024-12-28 11:44:01 +0530

    updated the css a bit better

--- simple_site_example/index.js
+++ simple_site_example/index.js
@@ -1,12 +1,14 @@
-const burgerIcon = document.querySelector(".burger-icon");
-const rightNav = document.querySelector(".right-nav");
+document.addEventListener("DOMContentLoaded", () => {
+  const burgerIcon = document.querySelector(".burger-icon");
+  const rightNav = document.querySelector(".right-nav");
 
-burgerIcon.addEventListener("click", () => {
-  rightNav.classList.toggle("active");
-});
+  burgerIcon.addEventListener("click", () => {
+    rightNav.classList.toggle("active");
+  });
 
-document.addEventListener("click", (event) => {
-  if (!rightNav.contains(event.target) && !burgerIcon.contains(event.target)) {
-    rightNav.classList.remove("active");
-  }
+  document.addEventListener("click", (event) => {
+    if (!rightNav.contains(event.target) && !burgerIcon.contains(event.target)) {
+      rightNav.classList.remove("active");
+    }
+  });
 });
